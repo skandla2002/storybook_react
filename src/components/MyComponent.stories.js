9966b562793fb82b572600e2b47bae2459fb934c
Andy Pak <skandla2002@gmail.com>
2021-01-22 23:49:59 +0900

    image 영역 등 외부 영역 구현 변경

--- src/components/MyComponent.stories.js
+++ src/components/MyComponent.stories.js
@@ -1,15 +1,33 @@
 import React from 'react';
-import { MyComponent } from './MyComponent';
+// import { MyComponent } from './MyComponent';
+
+
+// Component 사용시
+// export default {
+//     // title: 'MyComponent',
+//     // component: MyComponent,
+// };
+
+// const Template = (args) => <MyComponent {...args}></MyComponent>;
+
+// export const firstStory = Template.bind({});
+
+// firstStory.args = {
+//     // 필요한 인자값
+// }
+
+import imageFile from './static/image.png';
 
 export default {
-    title: 'MyComponent',
-    component: MyComponent,
+    // static resource 활용시
+    title: 'img'
 };
 
-const Template = (args) => <MyComponent {...args}></MyComponent>;
+const image = {
+    src: imageFile,
+    alt: 'my image',
+};
 
-export const firstStory = Template.bind({});
-
-firstStory.args = {
-    // 필요한 인자값
-}
+export const withAnImage = () => (
+    <img src={image.src} alt={image.alt} />
+)
